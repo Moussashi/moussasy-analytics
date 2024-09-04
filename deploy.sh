58f47a33f8cc8618190fb7e0a9543ece795d95b6
Moussashi <sy.moussa04@gmail.com>
2024-09-04 22:29:37 +0200

    second

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Moussashi/portfolio-pro.git main:gh-pages
+git push -f git@github.com:Moussashi/moussasy-analytics.git main:gh-pages
 
 cd -
