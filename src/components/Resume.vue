c28dd4768a6130b462e7cfed39e242838245b3ff
Moussashi <sy.moussa04@gmail.com>
2024-09-06 09:49:44 +0200

    update CV pro 2

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -61,7 +61,7 @@
 
             </section>
             </div>
-            <img src="../assets/CVproSY" class="CV" alt="CV pro">
+            <img src="../assets/CVproSY.png" class="CV" alt="CV pro">
 
         </div>
 
